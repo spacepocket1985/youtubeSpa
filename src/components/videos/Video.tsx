91f4e2c8305657886a528c3326fbf620539d3006
Space Pocket <space1985pocket@gmail.com>
2024-11-12 16:46:16 +0300

    refactor: update grid and list view

--- src/components/videos/Video.tsx
+++ src/components/videos/Video.tsx
@@ -1,10 +1,9 @@
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { VideoItem } from '../../service/YouTubeApi';
 import { ListMode } from './ListViewMode';
+import { Box } from '@mui/material';
 
 export const Video: React.FC<{ videoItem: VideoItem; viewMode: ListMode }> = ({
   videoItem,
@@ -16,44 +15,52 @@ export const Video: React.FC<{ videoItem: VideoItem; viewMode: ListMode }> = ({
     margin: 'none',
   };
   const itemStyle = viewMode === ListMode.Grid ? gridStyle : null;
+
+  const imgStyle: React.CSSProperties =
+    viewMode === ListMode.Grid
+      ? {
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+          marginBottom: '8px',
+          borderRadius: '5px',
+        }
+      : {
+          width: '120px',
+          height: '90px',
+          borderRadius: '5px',
+          marginRight: '8px',
+        };
+
   return (
-    <>
-      <ListItem alignItems="flex-start" sx={itemStyle}>
-        <ListItemAvatar>
-          <Avatar
-            alt="video img"
-            src={videoItem.snippet.thumbnails.medium?.url}
-            variant="square"
-            sx={{
-              width: 120,
-              height: 90,
-              mr: 0.5,
-              borderRadius: '5px',
-            }}
-          />
-        </ListItemAvatar>
-        <ListItemText
-          primary={videoItem.snippet.title}
-          secondary={
-            <>
-              <Typography
-                component="span"
-                variant="subtitle2"
-                sx={{ color: 'text.primary', display: 'block' }}
-              >
-                {videoItem.snippet.description}
-              </Typography>
-              <Typography
-                component="span"
-                variant="subtitle1"
-                sx={{ color: 'text.primary', display: 'block' }}
-              >
-                {videoItem.snippet.channelTitle}
-              </Typography>
-            </>
-          }
-        />
-      </ListItem>
-    </>
+    <ListItem alignItems="flex-start" sx={itemStyle}>
+      <Box
+        component="img"
+        sx={imgStyle}
+        alt="video thumbnail"
+        src={videoItem.snippet.thumbnails.medium?.url}
+      />
+      <ListItemText
+        primary={videoItem.snippet.title}
+        secondary={
+          <>
+            <Typography
+              component="span"
+              variant="subtitle2"
+              sx={{ color: 'text.primary', display: 'block' }}
+            >
+              {videoItem.snippet.description}
+            </Typography>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              sx={{ color: 'text.primary', display: 'block' }}
+            >
+              {videoItem.snippet.channelTitle}
+            </Typography>
+          </>
+        }
+      />
+    </ListItem>
   );
 };
